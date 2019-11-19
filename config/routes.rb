f82324e6942d2fbac4f8f9c5eb46e7b1181b707a
prfm0401 <perfume6820@gmail.com>
2019-11-19 18:13:41 +0900

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'static_pages/contact'
-  root 'static_pages#hello'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
